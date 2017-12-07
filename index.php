9c0caefae9be4084960fcd21d8a040a7f224202a
Luiz Eduardo Oliveira Fonseca <luizeof@gmail.com>
2017-12-07 10:15:44 -0200

    Ajustes gerais na API

--- index.php
+++ index.php
@@ -18,8 +18,10 @@ use Mautic\MauticApi;
 
 session_start();
 
+// a url do seu mautic
 $mauticUrl = "https://mkt.seumautic.com.br";
 
+// login do Basic Authentication
 $credentials = array(
     'userName'   => "login-do-mautic",
     'password'   => "senha-do-mautic"
@@ -35,30 +37,33 @@ $api = new MauticApi();
 // Nova instância do objeto Contact
 $contactApi = $api->newApi('contacts', $auth, $mauticUrl);
 
-// Captura os dados do POST
-$mautic_data = array(
-    'email'        =>    $_POST['email'],  // customize a variavel
-    'firstname'    =>    $_POST['name'],    // customize a variavel
-    'phone'		     =>    $_POST['phone']    // customize a variavel
-);
-
 // Pesquisa o contato pelo e-mail
 // "email:luiz@powertic.com"
+$email = $_POST['email'];
 $response = $contactApi->getList("email:$email");
 $json = json_encode($response);
 $decodedJson = json_decode($json, true);
 
-$id = 1;
-foreach ($decodedJson as $lista) {
-    foreach ($decodedJson["contacts"] as $listaTotal) {
-        $id = $listaTotal["id"];
-        break;
-    }
-    break;
+$id = 0;
+$mautic_data = array();
+
+foreach($decodedJson as $lista)
+{
+ 	foreach($decodedJson["contacts"] as $listaTotal)
+	{
+ 		 $id                          =     $listaTotal["id"];
+     $mautic_data                 =     $listaTotal;
+     // coloque todos os dados que você quer atualizar aqui
+     $mautic_data["email"]        =>    $_POST['email'],  // customize a variavel
+     $mautic_data["firstname"]    =>    $_POST['name'],    // customize a variavel
+     $mautic_data["phone"]		    =>    $_POST['phone']    // customize a variavel
+
+     break;
+ 	}
+	break;
 }
 
-// Permite criar um novo contato caso o contato especificado
-// não seja encontrado
+// Permite criar um novo contato caso o contato especificado não seja encontrado
 $createIfNotFound = true;
 
 // Envia a requisição para o Mautic atualizar ou criar o contato
